120bc39bda849f02d34105e3e11243d26a4ee8c6
chrisndo <50555471+chrisndo@users.noreply.github.com>
2019-11-23 17:40:09 -0500

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => (
     <div className="banner-text">
       <h1 className="responsive-headline">Christopher Do</h1>
       <h3>
-        <span>Cloud Security Engineer at <a href="https://www.bah.com/">Booz Allen Hamilton.</a></span>
+        <span>Cloud Security Engineer at <a href="https://www.deloitte.com/">Deloitte.</a></span>
         <br/><br/>
         <a className="smoothscroll" href="#about">
           {" "}
